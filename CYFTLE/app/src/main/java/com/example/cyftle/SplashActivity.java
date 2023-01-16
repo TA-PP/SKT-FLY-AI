92f58a8cb0e593bcc03da13e392fa919cc9d5aae
TA-PP <kgrewd@naver.com>
2023-01-16 17:43:54 +0900

    Studying Flask & Android changed

--- CYFTLE/app/src/main/java/com/example/cyftle/SplashActivity.java
+++ CYFTLE/app/src/main/java/com/example/cyftle/SplashActivity.java
@@ -1,31 +1,33 @@
 package com.example.cyftle;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-import android.os.Handler;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splash);
 
+        // 타이틀 바 숨기기
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        //로딩화면을 띄우고 다음 화면으로 넘어가게 하는 함수
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        
+        //로딩화면 시작.
+        Loadingstart();
+    }
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },1000);
+        },2000);
     }
 }
--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect        # 모듈 추가
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 
+
 @app.route('/')             # 경로를 생성
-def hello():                # 유일한 함수명 하나 설정한다.
-    return 'Hello, My Fist Flask!'
+def index():                # 유일한 함수명 하나 설정한다.
+    # return 'Hello, My Fist Flask!'
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        join = request.args.get('join')
+        return render_template('login.html', join=join)
+    elif request.method == 'POST':
+        userid = request.form['userid']
+        userpw = request.form['userpw']
+
+        if userid == 'admin' and userpw == '1234':
+            return redirect('/')
+        else:
+            return render_template('login.html', alertMsg='아이디 또는 비밀번호가 틀립니다.')
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if request.method == 'GET':
+        return render_template('join.html')
+    else:
+        userid = request.form['userid']
+        userpw = request.form['userpw']
+        name = request.form['username']
+        f = request.files['img1']
+        f.save('./upload/' + secure_filename(f.filename))
+
+        # DB 인젝션 공격이 있는 지 검토
+        # 이미 가입되어 있는 지 확인
+        # DB에 회원정보 저장하기
+        return redirect('/login?join=y')
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
