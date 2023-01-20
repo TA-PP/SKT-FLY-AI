3c08c409c1f3ce09c606e9155504ee3ab1d11329
TA-PP <kgrewd@naver.com>
2023-01-20 15:36:06 +0900

    Studying flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect        # 모듈 추가
+from flask import Flask, render_template, request, redirect, session, send_file        # 모듈 추가
 from werkzeug.utils import secure_filename
 from sqlalchemy import create_engine, text
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'winincow'           # session을 암호화 해주는 키 값 지정
 
 
 # config.py를 불러오는 코드
@@ -12,11 +13,18 @@ database = create_engine(app.config['DB_URL'], encoding = "utf-8")
 app.database = database
 
 
+# 인덱스
 @app.route('/')             # 경로를 생성
 def index():                # 유일한 함수명 하나 설정한다.
-    # return 'Hello, My Fist Flask!'
-    return render_template('index.html')
+    if 'ss_name' in session:
+        ss_name = session['ss_name']
+        ss_img = session['ss_img']
+    else:
+        ss_name = ""  
+        ss_img = ""          
+    return render_template('index.html', ss_name=ss_name, ss_img=ss_img)
 
+# 로그인
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'GET':
@@ -26,34 +34,101 @@ def login():
         userid = request.form['userid']
         userpw = request.form['userpw']
 
-        user = app.database.execute(text("""
-            select name from mem where 
-            userid = :userid and userpw = SHA2(:userpw, 256);"""), {
-                'userid' : userid,
-                'userpw' : userpw
-            }).fetchone()
-        return user['name']
-        
-        #if userid == 'admin' and userpw == '1234':
-        #    return redirect('/')
-        #else:
-        #    return render_template('login.html', msg='아이디 또는 비밀번호가 틀립니다.')
+        # 회원가입하는 부분
+        sql = f"""select num, name, img from mem where 
+        userid = '{userid}' and 
+        userpw = SHA2('{userpw}', 256);"""
+        user = app.database.execute(text(sql)).fetchone()
 
-@app.route('/join', methods=['GET', 'POST'])
+        if user:
+            session['ss_id'] = user['num']
+            session['ss_userid'] = userid
+            session['ss_name'] = user['name']
+            session['ss_img'] = user['img']
+            return redirect('/')            
+        else:
+            return render_template('login.html', msg='아이디 또는 비밀번호가 틀립니다.')
+
+# 로그아웃
+@app.route('/logout')
+def logout():
+    session['ss_id'] = False
+    session['ss_userid'] = False
+    session["ss_name"] = False
+    session["ss_img"] = False
+    return redirect('/')
+    
+# 회원가입    
+@app.route('/join', methods=['GET','POST'])
 def join():
     if request.method == 'GET':
         return render_template('join.html')
-    else:
+    else: # POST
         userid = request.form['userid']
         userpw = request.form['userpw']
-        name = request.form['username']
-        f = request.files['img1']
-        f.save('./upload/' + secure_filename(f.filename))
+        name = request.form['name']
+        sex = request.form['sex']
+        post_num = request.form['post_num']
+        address = request.form['address']
+        tel = request.form['tel']
+        age = request.form['age']
 
-        # DB 인젝션 공격이 있는 지 검토
-        # 이미 가입되어 있는 지 확인
+        f = request.files['img1']
+        f.save("Flask/static/" + secure_filename(f.filename))
+        img = f.filename
+
+        # DB 인젝션 공격이 있는 검토
+        # 이미 가입 되어 있는지 확인
         # DB에 회원정보 저장하기
+        app.database.execute(text("""
+        insert into mem (
+            userid, userpw, name, sex, post_num, address, tel, age, img
+        ) values (
+            :userid, SHA2(:userpw, 256), :name, :sex, :post_num, :address, :tel, :age, :img);"""), {
+                'userid' : userid,
+                'userpw' : userpw,
+                'name' : name,
+                'sex' : sex,
+                'post_num' : post_num,
+                'address' : address,
+                'tel' : tel,
+                'age' : age,
+                'img' : img,
+            }).lastrowid
         return redirect('/login?join=y')
+
+# 리스트
+@app.route('/list')
+def list_members():
+    sql = """select num, userid, name, sex, address, age, img from mem;"""
+    user_list = app.database.execute(text(sql)).all()
+
+    print(session)
+    return render_template('list.html', user_list=user_list, session_id=session['ss_id'])
+
+# 리스트 수정
+@app.route('/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    return redirect('/list')
+
+# 리스트 삭제
+@app.route('/delete', methods=['GET'])
+def delete():
+    num = request.args.get('num')
+    sql = "delete from mem where num = %s and userid='%s'" % (num, 
+    session['ss_userid'])
+
+    print(sql)
+    app.database.execute(text(sql)).lastrowid    
+    # 사진파일 삭제 (세션)
+    return redirect('/logout')
+
+@app.route('/cdn/<path:filename>')
+def get_img(filename):
+    if filename == 'default_profile.png':
+        return send_file('static/default_profile.png')
+    else:
+        return send_file('upload/' + secure_filename(filename))
 
 @app.route("/insert")
 def insertdb():
--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 툴바에 홈 버튼 활성화
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // 툴바의 홈버튼의 이미지를 변경(기본 이미지는 뒤로가기 화살표)
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_background);
     }
 }
